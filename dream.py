461e27c1a6eeb6da3e2ac9f952e1cbc22d781227
Alexander Sobolev <ru.sobolev@gmail.com>
2021-10-05 16:27:50 +0300

    Modified init.py and added file with my dream

---
+++ dream.py
@@ -0,0 +1,1 @@
+print("I want to become a DevOps engineer and get positon with 3000$ per month ;)");
--- init.py
+++ init.py
@@ -1,1 +1,2 @@
 print("Hello World from Kazan!");
+print("Author: Alexander Sobolev, DevOps-15");
